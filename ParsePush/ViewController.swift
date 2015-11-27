5224eb8bfeebdfb0c262d734ecc0f157d54a9f25
Tanmay Bakshi <tajymany@gmail.com>
2015-11-26 23:14:09 -0500

    Initial Commit

---
+++ ParsePush/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParsePush
+//
+//  Created by Tanmay Bakshi on 2015-11-26.
+//  Copyright © 2015 Tanmay Bakshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
